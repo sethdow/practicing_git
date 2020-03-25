b6966a2a0a51545bbc6fa902c1689092a0e3bb5f
Seth Dow <sdow@sbacademy.org>
2020-03-25 20:14:13 +0100

    now the master and the other branch should have a merge conflict

--- file1.py
+++ file1.py
@@ -3,5 +3,6 @@
 import sys
 
 print("This program prints the version of python in the default env")
+print("hmm, until you make a commit, it doesn't matter which branch you are in, this is the master")
 
 print(sys.version)
