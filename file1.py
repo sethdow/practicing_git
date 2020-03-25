153d85b9e27e70fddefe33974d0c466dd771d647
Seth Dow <sdow@sbacademy.org>
2020-03-25 20:12:46 +0100

    Made hopefully some conflicting changes

--- file1.py
+++ file1.py
@@ -3,5 +3,7 @@
 import sys
 
 print("This program prints the version of python in the default env")
+print(" I made this edit in the master branch")
+print(" I made this edit in the other branch")
 
 print(sys.version)
